41d74804cd2253c4f8f2e509d8c60b59d47cb2ea
alirfanyasin <risayunita21@gmail.com>
2024-04-22 09:50:34 +0700

    file colocation

---
+++ src/app/colocation/page.tsx
@@ -0,0 +1,14 @@
+// Components
+function Sidebar() {
+  return <h5>Side Bar</h5>
+}
+
+
+export default function pages() {
+  return (
+    <div>
+      <h1>Page</h1>
+      <Sidebar/>
+    </div>
+  )
+}
